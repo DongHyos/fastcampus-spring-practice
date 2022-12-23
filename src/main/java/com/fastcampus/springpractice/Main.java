d578760986646806b9886b4991313e2b39c73bab
DongHyos <dong87070@naver.com>
2022-12-23 12:28:11 +0900

    Initialize #1 - java project
    지난 시간 자바 애플리케이션에 스프링을 적용해본다.
    
    - https://github.com/DongHyos/fastcampus-java-oop

---
+++ src/main/java/com/fastcampus/springpractice/Main.java
@@ -0,0 +1,15 @@
+package com.fastcampus.springpractice;
+
+import com.fastcampus.springpractice.logic.BubbleSort;
+import com.fastcampus.springpractice.logic.JavaSort;
+import com.fastcampus.springpractice.logic.Sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Sort<String> sort = new JavaSort<>();
+
+        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+    }
+}
